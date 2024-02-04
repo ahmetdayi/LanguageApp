2a2632cd16684e4a614c0de5055652645dfe3435
fullan42 <hasan_-1@outlook.com>
2024-02-04 17:56:57 +0000

    readme.md added

--- src/main/java/com/cs491/languageapp/LanguageAppApplication.java
+++ src/main/java/com/cs491/languageapp/LanguageAppApplication.java
@@ -3,14 +3,28 @@ package com.cs491.languageapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
+import javax.print.Doc;
 
 
 @SpringBootApplication()
 
 public class LanguageAppApplication {
-
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 	public static void main(String[] args) {
+
 		SpringApplication.run(LanguageAppApplication.class, args);
 
 
